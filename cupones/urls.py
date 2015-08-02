ee0ef265434e19d48c864fb898adfefe947dcb41
Jose Sanchez <chinoxyz@gmail.com>
2015-08-02 18:32:50 -0430

    initial template works now

--- cupones/urls.py
+++ cupones/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cupones.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    #url(r'^/$', CouponListView.as_view()),
+    url(r'^$', CouponListView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
